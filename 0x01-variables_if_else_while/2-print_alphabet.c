4ba144293339207ea0a4e3cc169f5efe0403fa11
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-09-11 21:41:23 +0300

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase.
+ * Return: 0 on success
+ */
+int main(void)
+{
+char c = 'a;'
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
