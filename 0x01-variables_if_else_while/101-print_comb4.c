f1e295d7d37ff9b695d4324b1025543960494e7c
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-09-11 23:29:40 +0300

    Print comb 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible different combination of three digits.
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i = '0';
+int j = '0';
+int k = '0';
+while (i <= '7')
+{
+while (j <= '8')
+{
+while (k <= '9')
+{
+if (i < j && j < k)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (!(i == '7' && j == '8' && k == '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+k++;
+}
+k = '0';
+j++;
+}
+j ='0';
+i++;
+}
+putchar('\n');
+return (0);
+}
