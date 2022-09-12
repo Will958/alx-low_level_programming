016677541ef4aa8d83c168643e57799dbbf386fc
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-09-12 08:23:41 +0300

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,6 @@
  */
 int main(void)
 {
-    char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, strl, 59);
+write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-1\n", 59);
     return (1);
 }
