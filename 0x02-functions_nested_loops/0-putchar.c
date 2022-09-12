e226b385b0159215cbb4729a00c5b670e8746b19
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-09-12 08:36:07 +0300

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints _putchar
+ * Return: 0 on success
+ */
+int main(void)
+{
+char c[]= "_putchar";
+int i;
+for (i = 0; i < 8;i++)
+{
+_putchar(c[i]);
+} 
+_putchar('\n');
+return (0);
+}
