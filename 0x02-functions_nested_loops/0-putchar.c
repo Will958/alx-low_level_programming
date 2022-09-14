4512481a6344585f17c8c84c98b2454394713ad8
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-09-14 18:55:30 +0300

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-char c[]= "_putchar";
+char c[] = "_putchar";
 int i;
-for (i = 0; i < 8;i++)
+for (i = 0; i < 8; i++)
 {
 _putchar(c[i]);
-} 
+}
 _putchar('\n');
 return (0);
 }
