a7a7e901a523521495ba1b7dcd6c5aaf8603b98d
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-09-14 21:38:05 +0300

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
  */
 void times_table(void)
 {
-int i, j,res;
+int i, j, res;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
@@ -16,7 +16,7 @@ if (j != 0)
 _putchar(',');
 _putchar(' ');
 }
-if (res >=10)
+if (res >= 10)
 {
 _putchar((res / 10) + '0');
 _putchar((res % 10) + '0');
