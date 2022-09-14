eed8b250d4a093a01ef78ce382e184ca436610ec
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-09-14 19:00:13 +0300

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints a-z
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
