7497f0d66cdf02eaceea68fcf02b1596de98beb2
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-09-15 12:38:09 +0300

    Largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,21 +9,20 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a > b && a > c)
+if (a > b)
 {
+if (a > c)
 largest = a;
+else
+largest = c;
 }
-else if (b > c && b > a)
+else if (b > c)
 {
 largest = b;
-}
-else if (c > b)
-{
-largest = c;
 }
 else
 {
-largest = b;
+largest = c;
 }
 return (largest);
 }
