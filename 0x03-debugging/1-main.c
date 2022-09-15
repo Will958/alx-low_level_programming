a9805b30f2e948f3269cb0a22528e59b5ca4e477
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-09-15 10:55:24 +0300

    Main 2

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * main - cause an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/* while (i < 10)
+{
+    putchar(i);
+} */
+printf("Infinite loop avoided! \\0/\n");
+return (0);
+}
