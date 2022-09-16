6e4c5e415270cfef86ccc7035efa9d54ad841494
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-09-16 11:12:16 +0300

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0 - 9
+ * _putchar - prints the values
+ */
+void print_numbers(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+{
+_putchar (1 + '0');
+}
+_putchar ('\n');
+}
