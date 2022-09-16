ea7e252cb0cba5f3d01715a7c966836bc18e72bd
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-16 18:38:36 +0300

    Print remaining days

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * main - test function that prints if integer is positive or negative.
- * Return: 0
- */
+* main - main
+*
+* Return: 0
+*/
 int main(void)
 {
 int i;
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,20 +9,21 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a > b)
+if (a > b && a > c)
 {
-if (a > c)
 largest = a;
-else
-largest = c;
 }
-else if (b > c)
+else if (b > c && b > a)
 {
 largest = b;
 }
-else
+else if (c > b)
 {
 largest = c;
+}
+else
+{
+largest = b;
 }
 return (largest);
 }
