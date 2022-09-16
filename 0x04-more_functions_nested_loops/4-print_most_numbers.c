bc3834194bfbaba4ee7976f5c9e6fc04f92d8691
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-16 14:03:55 +0300

    Print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main..h"
+/**
+ * print_most_numbers - function that print the numbers from 0 - 9
+ * 
+ */
+void print_most_numbers(void)
+{
+int a
+for (a = 0; a <= 9; a++)
+{if (!(a = 2 && a = 4))
+_putchar (a + '0');
+}
+
+_putchar ('\n');
+}
