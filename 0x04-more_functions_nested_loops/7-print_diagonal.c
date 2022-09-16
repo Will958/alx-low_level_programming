d958cf3f828b22c3e6ea8c5404f60c2c9cfdfe2c
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-16 14:58:41 +0300

    Print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,9 +8,9 @@ void print_diagonal(int n)
 int i, j;
 if (n > 0)
 {
-for (i = 1; i <= n; i++)
+for (j= 1; j <= n; j++)
 {
-for (j = 1; j <= i; j++)
+for (i = 1; i <= j; i++)
 {
 if (i != 1)
 _putchar(' ');
