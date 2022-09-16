cbef944746b6610670dfbf26e5197f6279d57cf6
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-16 15:41:41 +0300

    Print squares

---
+++ 0x04-more_functions_nested_loops/8-print_squares.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_squares - function that prints a square.
+ * @size: the size of the square.
+ * _putchar - function to print # and newline.
+ */
+void print_square(int size)
+{
+int i;
+int j;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 1; j < size; j++)
+{
+_putchar('#');
+}
+_putcha('#');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
