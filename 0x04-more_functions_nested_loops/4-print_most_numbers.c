b13a75abaa826586de963f6f368afb394efa9c05
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-16 14:07:19 +0300

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,12 @@
  */
 void print_most_numbers(void)
 {
-int a
-for (a = 0; a <= 9; a++)
-{if (!(a = 2 && a = 4))
+char a
+for (a = '0'; a <= '9'; a++)
+{if (!(a = '2' && a = '4'))
+{
 _putchar (a + '0');
 }
-
+}
 _putchar ('\n');
 }
