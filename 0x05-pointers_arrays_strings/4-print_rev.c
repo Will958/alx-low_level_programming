eeeda6a914421aefcbfbb3bcd6d862a11526ab62
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-21 07:53:56 +0300

    Print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints string in reverse
+ * @s: string
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+{
+int i = 0;
+while (*s != '\0')
+{
+s++;
+i++;
+}
+while (i > 0)
+{
+s--;
+_putchar(*s);
+i--;
+}
+_putchar('\n')
+}
+}
