a6d8ebc859321c74ed1f4d6ffc1d83f1e7e4b908
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-21 08:45:07 +0300

    Puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,22 +5,22 @@
  */
 void puts_half(char *str)
 {
-int len, n, i;
-len = 0;
-while (str[len] != '0')
+int m, n, i;
+m = 0;
+while (str[m] != '0')
 {
-len++;
+m++;
 }
-if (len % 2 == 0)
+if (m % 2 == 0)
 {
-for (i = len / 2; str[i] != '\0'; i++)
+for (i = m / 2; str[i] != '\0'; i++)
 {
 _putchar(str[i]);
 }
 }
-else if (len % 2)
+else if (m % 2)
 {
-for (n = (len - 1) / 2; n < len - 1; n++)
+for (n = (m - 1) / 2; n < m - 1; n++)
 {
 _putchar(str[n + 1]);
 }
