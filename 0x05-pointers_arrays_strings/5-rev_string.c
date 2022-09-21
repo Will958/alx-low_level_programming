f245234540b7c795269ae0cb2d34c0bfa312a34e
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-21 08:02:35 +0300

    Rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+chat *t = s;
+char n[1000];
+short c = 0;
+while (*s != '\0')
+{
+n[c] = *s;
+s++;
+c++;
+}
+c = 0;
+while (s > t)
+{
+s--;
+*s = n[c];
+c++;
+}
+}
