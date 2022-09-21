3692f34321e4c44bb83545fe5d09196ff034f31a
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-21 14:56:57 +0300

    Atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - converts a string to an integer
+ * @s: string
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+int a, b;
+unsigned int x;
+int y = 1;
+char z;
+a = 0;
+x = 0;
+while (*(s + a) != '\0')
+{
+z = *(s + a);
+if (z == '-')
+{
+y *= -1;
+}
+if (z >= '0' && c <= '9')
+{
+a =b;
+while (*(s + b) > 47 && *(s + b) < 58)
+{
+a = (a * 10) + *(s + b) - '0';
+b++;
+}
+break;
+}
+a++;0
+}
+if (y < 0)
+x *= y;
+return (x);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ int _putchar(char c);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
