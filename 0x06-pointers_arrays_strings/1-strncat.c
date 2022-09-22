58fd9d84cc73c30e55e1aef1ecf9aed029d6089b
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-22 17:25:09 +0300

    Strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,15 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
 i = 0;
-for (j = 0; dest[j] != '\0'; j++)
-while (src[i] != '\0' && i < n)
+while (dest[i] != '\0')
 {
-dest[j + i] = src[i];
 i++;
 }
+for (j = 0; j < n && src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
+dest[i] = '\0';
 return (dest);
 }
