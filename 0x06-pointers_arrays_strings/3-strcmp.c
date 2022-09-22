e9bc52cb22ce489daffa782ccff5ba6e3eddc17c
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-22 19:22:40 +0300

    Strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _strcmp - compares two strings
+ * _strcmp - compares two strings ending with first difference char
  * @s1: string to be compared
  * @s2: string to compair
- * Return: integer
+ * Return: difference between the first charater that are not the same
  */
 int _strcmp(char *s1, char *s2)
 {
 int a;
-for (i =0; s1[a] != '\0' && s2[a] !='\0' && s1[a] == s2[a]; a++);
+for (i = 0; s1[a] != '\0' && s2[a] != '\0' && s1[a] == s2[a]; a++);
 return (s1[a - s2[a]]);
 }
