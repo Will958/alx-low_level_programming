b59ea5a0119c51aca157cba37d14a2428a553b95
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-22 18:51:31 +0300

    Strcmp

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 int _putchar(char c);
-char *_strc(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *s1, char *s2);
+char *_strncpy(char *s1, char *s2, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
