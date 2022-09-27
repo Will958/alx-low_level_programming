bb309f8049444404de07da77806f1e49fcdf30e1
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-09-27 11:59:43 +0300

    Memset

--- 0x07-pointer_arrays_strings/0-memset.c
+++ 0x07-pointer_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @n: number of bytes filled
  * Return: s
  */
-char *memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 for (i = 0; i < n; i++)
