d8760b7d3fd6248e9d0a7c4f92e1d4797cba1f1f
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-10-02 12:35:40 +0300

    Whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  *@argv: array of strings
  *Return: 0 on success
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+argc--;
 printf("%s\n", argv[0]);
 return (0);
 }
