9ee6d42443d4bd50941e8c03419c34a0e6056614
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-10-02 11:46:20 +0300

    Add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *main - adds all arguments together if they are digits
+ *@args: argument count only accepts ints separated by spaces
+ *@argv: argument vector
+ *Return: 0 on success, 1 0n failure
+ */
+int main(int argc, char *argv[])
+{
+int s, i, j;
+s = 0;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+}
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+s += atoi(argv[i]);
+}
+printf("%d\n", s);
+return (0);
+}
