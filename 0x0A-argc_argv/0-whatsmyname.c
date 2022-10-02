4202d089865592fd0cb67fe29c8733fe568447a8
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-10-02 10:35:22 +0300

    Whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - prints it's name
+ *@argc: count of argument in argv[]
+ *@argv: array of strings
+ *Return: 0 on success
+ */
+int main(int argc _attribute_((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
