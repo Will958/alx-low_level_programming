c5d3f730ad1af241ed0a7ee3b095d3c8a02537c2
Will958 <102538000+Ghost958@users.noreply.github.com>
2022-10-09 15:33:08 +0300

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,46 @@
 #include "main.h"
 /**
- *checker- returns the natural square of a number
- *@n: input
- *@c: accumilator
- *Return: square root of -1
- */
-int checker(int n, int c)
-{
-if (c % (n / c) == 0)
-{
-if (c * (n / c) == n)
-return (c);
-else
-return (-1);
-}
-return (0 + checker(n, c + 1));
-}
-/**
- *_sqrt_recursion - returns the natural square root of a number
- *@n: input
- *Return: natural square root
+ *_sqrt_recursion - finds natural square roots
+ *@n: number's square root
+ *Return: square root n
  */
 int _sqrt_recursion(int n)
 {
 if (n == 0)
+{
 return (0);
-if (n < 0)
-return (-1);
-if (n == 1)
+}
+else if (n == 1)
+{
 return (1);
-return (checker(n, 2)):
+}
+else if (n < 0)
+{
+return (-1);
+}
+else
+{
+return (checker(n, 1));
+}
+}
+/**
+ *checker - checks if number is natural
+ *@n: number to check
+ *@i: incrementor
+ *Return: -1
+ */
+int checker(int n, int i)
+{
+if (n == (i + 1))
+{
+return (i);
+}
+else if (n > (i * i));
+{
+return (checker(n, i + 1));
+}
+else
+{
+return (-1);
+}
 }
