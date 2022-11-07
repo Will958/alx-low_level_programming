e1bc969d097d23c0187742cf8faba9eab3839eca
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-11-07 10:19:30 +0300

    Variadic function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,71 +1,51 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
+
 /**
- * p_char - prints characters
- * @c: arguments
- */
-void p_char(va_list c)
-{
-printf("%c", va_arg(c, int));
-}
-/**
- * p_int - print integers
- * @a: arguments
- */
-void p_int(va_list i)
-{
-printf("%d", va_arg(i, int));
-}
-/**
- * p_float - prints float values
- * @a: arguments
- */
-void p_float(va_list a)
-{
-printf("%f", va_arg(a, double));
-}
-/**
- * p_string - prints strings
- * @a: arguments
- */
-void p_string(va_list p)
-{
-char *st = va_arg(p, char *);
-st == NULL ? printf("(nil)") : printf("%s", st);
-}
-/**
- * print_all - prints any type
- * @format: format symbols in order
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ * Return: no return.
  */
 void print_all(const char * const format, ...)
 {
-va_list ap;
-unsigned int i, j;
-i = j = 0;
-char *s = "";
-printType type[] = {
-{ "c", p_char },
-{ "i", p_int },
-{ "f", p_float },
-{ "s", p_string },
-{NULL, NULL}
-};
-va_start(ap, format);
+va_list valist;
+unsigned int i = 0, j, c = 0;
+char *str;
+const char t_arg[] = "cifs";
+va_start(valist, format);
 while (format && format[i])
 {
 j = 0;
-while (j < 4)
+while (t_arg[j])
 {
-if (*type[j].type == format[i])
+if (format[i] == t_arg[j] && c)
 {
-printf("%s", s);
-type[j].b(ap);
-s = ", ";
+printf(", ");
+break;
 }
 j++;
 }
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(valist, int)), c = 1;
+break;
+case 'i':
+printf("%d", va_arg(valist, int)), c = 1;
+break;
+case 'f':
+printf("%f", va_arg(valist, double)), c = 1;
+break;
+case 's':
+str = va_arg(valist, char *), c = 1;
+if (!str)
+{
+printf("(nil)");
+break;
+}
+printf("%s", str);
+break;
+}
 i++;
 }
-printf("\n");
+printf("\n"), va_end(valist);
 }
